2df36f1d1f679d8a27b5c1240dc5f89610d96822
Mason Edgar <Mason.Edgar@viasat.com>
2017-06-06 16:06:54 -0700

    Full Functioning GUI
    GUI should be fully operational. Still working on the back end of things though.

--- Python Scripts/File IO/GUI.py
+++ Python Scripts/File IO/GUI.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'GUI.ui'
-#
-# Created by: PyQt5 UI code generator 5.6
-#
-# WARNING! All changes made in this file will be lost!
+'''
+ Author: Mason Edgar
+ Supervisor: John Sochacki 
+ Graphical User Interface (GUI) for the Wifi Programmable Frequency Synthesizer
+ 
+'''
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QLineEdit
 
 
 class Ui_Dialog(object):
+    
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(405, 194)
@@ -22,6 +22,8 @@ class Ui_Dialog(object):
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
         self.lineEdit.setGeometry(QtCore.QRect(20, 80, 261, 31))
         self.lineEdit.setObjectName("lineEdit")
+        QLineEdit.setPlaceholderText(self.lineEdit, "Search for the file using the 'Browse' button")
+        QLineEdit.setReadOnly (self.lineEdit, True)
         self.BrowseButton = QtWidgets.QPushButton(Dialog)
         self.BrowseButton.setGeometry(QtCore.QRect(290, 80, 93, 28))
         self.BrowseButton.setObjectName("BrowseButton")
@@ -38,6 +40,7 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         self.OKbuttonBox.accepted.connect(Dialog.accept)
+        self.OKbuttonBox.accepted.connect(self.OKbuttonAccepted)
         self.OKbuttonBox.rejected.connect(Dialog.reject)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
@@ -47,10 +50,14 @@ class Ui_Dialog(object):
         self.BrowseButton.setText(_translate("Dialog", "Browse"))
         self.label.setText(_translate("Dialog", "Wifi Programmable Frequency Synthesizer Module"))
                
-        
     def BrowseFunction(self):
-        self.filename, _ = QFileDialog.getOpenFileName(None,"Open File","","Text Files (*.txt);;All Files (*)", None)
+        self.buffer, _ = QFileDialog.getOpenFileName(None,"Open File","","Text Files (*.txt);;All Files (*)", None)
+        QLineEdit.setText(self.lineEdit, self.buffer)
         
+
+    def OKbuttonAccepted(self):
+        self.filename = self.buffer
+        print(self.filename)
 
     
         
@@ -65,34 +72,34 @@ if __name__ == "__main__":
  
     
     
-    print("\n")
-    txt = open(self.filename,"r")
-    values = txt.readlines()
-
-    new_values = [None] * 13      
-    numbers = [None] * 13
-    
-    for i in range(len(values)):
-        
-        for index, char in enumerate(values[i]):
-    
-            if i < 12:
-                if char == '0' and index > 2:
-                    
-                    new_values[i] = values[i][index:-1]
-                
-                    break
-            else: 
-                if char == '0' and index > 2:
-                
-                    new_values[i] = values[i][index:]
-                
-                    break
-            
-    for i in range(len(new_values)):
-        
-        numbers[i] = int(new_values[i], 16)
-        
-        
-    for i in range(len(numbers)):
-        print(hex(numbers[i]))
+#    print("\n")
+#    txt = open(self.filename,"r")
+#    values = txt.readlines()
+#
+#    new_values = [None] * 13      
+#    numbers = [None] * 13
+#    
+#    for i in range(len(values)):
+#        
+#        for index, char in enumerate(values[i]):
+#    
+#            if i < 12:
+#                if char == '0' and index > 2:
+#                    
+#                    new_values[i] = values[i][index:-1]
+#                
+#                    break
+#            else: 
+#                if char == '0' and index > 2:
+#                
+#                    new_values[i] = values[i][index:]
+#                
+#                    break
+#            
+#    for i in range(len(new_values)):
+#        
+#        numbers[i] = int(new_values[i], 16)
+#        
+#        
+#    for i in range(len(numbers)):
+#        print(hex(numbers[i]))
