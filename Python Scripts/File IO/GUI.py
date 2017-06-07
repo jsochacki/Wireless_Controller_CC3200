1d2d9651de4d8ea3de3b09306f514301ce88a8ff
Mason Edgar <Mason.Edgar@viasat.com>
2017-06-07 15:57:52 -0700

    Incorporated IP config function to GUI
    The GUI now allows the user to set the IP address of the server so the client program can connect to whatever server it wants

--- Python Scripts/File IO/GUI.py
+++ Python Scripts/File IO/GUI.py
@@ -12,27 +12,27 @@ import resource_rc
 
 filename = ''
 flag = 0
+IPaddress = ''
 
 class Ui_Dialog(object):
-    
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(523, 236)
+        Dialog.resize(523, 296)
         Dialog.setAutoFillBackground(False)
         Dialog.setStyleSheet("QDialog{\n""background-color: rgb(255, 255, 255);\n""}")
         self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName("gridLayout")
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
         self.lineEdit.setObjectName("lineEdit")
-        self.gridLayout.addWidget(self.lineEdit, 4, 0, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit, 5, 0, 1, 1)
         self.BrowseButton = QtWidgets.QPushButton(Dialog)
         self.BrowseButton.setObjectName("BrowseButton")
-        self.gridLayout.addWidget(self.BrowseButton, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.BrowseButton, 5, 1, 1, 1)
         self.OKbuttonBox = QtWidgets.QDialogButtonBox(Dialog)
         self.OKbuttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.OKbuttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.OKbuttonBox.setObjectName("OKbuttonBox")
-        self.gridLayout.addWidget(self.OKbuttonBox, 5, 0, 1, 2)
+        self.gridLayout.addWidget(self.OKbuttonBox, 6, 0, 1, 2)
         self.label = QtWidgets.QLabel(Dialog)
         font = QtGui.QFont()
         font.setFamily("Cambria")
@@ -45,11 +45,25 @@ class Ui_Dialog(object):
         self.label.setWordWrap(True)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 3, 0, 1, 2)
+        self.IPlineEdit = QtWidgets.QLineEdit(Dialog)
+        self.IPlineEdit.setStyleSheet("")
+        self.IPlineEdit.setClearButtonEnabled(False)
+        self.IPlineEdit.setObjectName("IPlineEdit")
+        self.gridLayout.addWidget(self.IPlineEdit, 4, 0, 1, 1)
+        self.ConfirmButton = QtWidgets.QPushButton(Dialog)
+        self.ConfirmButton.setObjectName("ConfirmButton")
+        self.gridLayout.addWidget(self.ConfirmButton, 4, 1, 1, 1)
+        
+        
+        
         #################### BUTTON ACTION #########################################################
         self.BrowseButton.clicked.connect(self.BrowseFunction) 
+        self.ConfirmButton.clicked.connect(self.ConfirmFunction) 
         QLineEdit.setPlaceholderText(self.lineEdit, "Search for the file using the 'Browse' button")
         QLineEdit.setReadOnly (self.lineEdit, True)
         ############################################################################################
+        
+        
         self.retranslateUi(Dialog)
         self.OKbuttonBox.accepted.connect(Dialog.accept)
         self.OKbuttonBox.accepted.connect(self.OKbuttonAccepted)
@@ -59,13 +73,22 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Wifi Module"))
+        self.lineEdit.setPlaceholderText(_translate("Dialog", "Use the \'Browse\' button to search for the desired register file"))
         self.BrowseButton.setText(_translate("Dialog", "Browse"))
         self.label.setText(_translate("Dialog", "<html><head/><body><p><img src=\":/img/0ed03b37.png\"/></p><p><span style=\" font-weight:600;\">Wifi Programmable Frequency Synthesizer Module</span></p></body></html>"))
+        self.IPlineEdit.setPlaceholderText(_translate("Dialog", "Enter the IP address of the desired server then hit \'Confirm\'"))
+        self.ConfirmButton.setText(_translate("Dialog", "Confirm"))
                
     def BrowseFunction(self):
         self.buffer, _ = QFileDialog.getOpenFileName(None,"Open File","","Text Files (*.txt);;All Files (*)", None)
         QLineEdit.setText(self.lineEdit, self.buffer)
-        
+       
+    def ConfirmFunction(self):
+        global IPaddress
+        self.ConfirmButton.setText("Confirmed!")
+        self.IPlineEdit.setStyleSheet("color: rgb(0, 170, 0);")
+        IPaddress = QLineEdit.text(self.IPlineEdit)
+    
     def OKbuttonAccepted(self):
         global filename
         global flag
